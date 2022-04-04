72ca083fd45afedd20bc5f8e40902c3ba98fd1f7
Segopotso Makhutja <segopotso.makhutja@sovtech.com>
2022-04-04 19:56:23 +0200

    adds FilterForm solution

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -3,10 +3,9 @@ import React from 'react'
 export const FilterForm = (props) => {
 
   const onPriceInputChange = (e) => {
-    props.onPriceInputChange(e.target.name, e.target.value);
+    props.onPriceInputChange(e.target.name, parseInt(e.target.value));
   }
 
-  // TODO: bind handlers and props
   return (
     <div>
       <label htmlFor="priceFrom">Price From:</label>
@@ -14,13 +13,19 @@ export const FilterForm = (props) => {
         type="number"
         id="priceFrom"
         name="priceFrom"
-        placeholder="Price from..." />
+        placeholder="Price from..."
+        value={props.priceFrom || ''}
+        onChange={onPriceInputChange}
+      />
       <label htmlFor="priceTo">Price To:</label>
       <input
         type="number"
         id="priceTo"
         name="priceTo"
-        placeholder="Price to..." />
+        placeholder="Price to..."
+        value={props.priceTo || ''}
+        onChange={onPriceInputChange}
+      />
     </div>
   )
 }
